a9a29f9a5bc9d417641acefb8265fb9f89344f7b
BryanCodeDev <tiagotroller021019@gmail.com>
2026-08-14 10:32:05 -0500

    feat: Actualizacion

--- src/index.js
+++ src/index.js
@@ -41,7 +41,10 @@ function isAllowedOrigin(origin) {
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (isAllowedOrigin(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    if (origin) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Vary', 'Origin');
+    }
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
